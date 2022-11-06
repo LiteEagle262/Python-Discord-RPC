4d292f1780bc81c325b6a19c8ca301d7b4291c75
LiteEagle <48303729+LiteEagle262@users.noreply.github.com>
2022-11-05 21:21:36 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -9,6 +9,7 @@ while playerGui:FindFirstChild("LoadingGate") do
 end
 while not player do
 end
+wait(5)
 queueonteleport = syn.queue_on_teleport or queue_on_teleport
 player.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
