289e4fa32b0508bff88f8ca8651865b95fdfc17d
LiteEagle <48303729+LiteEagle262@users.noreply.github.com>
2022-11-05 20:44:31 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -7,62 +7,61 @@ local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/
 
 while playerGui:FindFirstChild("LoadingGate") do
     print("gay loading screen")
-else
-    queueonteleport = syn.queue_on_teleport or queue_on_teleport
-    player.OnTeleport:Connect(function(State)
-        if State == Enum.TeleportState.Started then
-            queueonteleport([[
-                loadstring(game:HttpGet('https://raw.githubusercontent.com/LiteEagle262/Python-Discord-RPC/main/test.lua'))()
-            ]])
-        end
-     end)
+end
+queueonteleport = syn.queue_on_teleport or queue_on_teleport
+player.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.Started then
+        queueonteleport([[
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/LiteEagle262/Python-Discord-RPC/main/test.lua'))()
+        ]])
+    end
+ end)
 
-    local OtherServers = httpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/2317712696/servers/Public?sortOrder=Asc&limit=100"))
-    local function joinNew()
-        if not isfile('servers.json') then
-            writefile('servers.json',httpService:JSONEncode({}))
-        end
-        local dontJoin = readfile('servers.json')
-        dontJoin = httpService:JSONDecode(dontJoin)
+local OtherServers = httpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/2317712696/servers/Public?sortOrder=Asc&limit=100"))
+local function joinNew()
+    if not isfile('servers.json') then
+        writefile('servers.json',httpService:JSONEncode({}))
+    end
+    local dontJoin = readfile('servers.json')
+    dontJoin = httpService:JSONDecode(dontJoin)
 
-        for _, Server in next, OtherServers["data"] do
-            if Server ~= game.JobId then
-                local j = true
-                for a,c in pairs(dontJoin) do
-                   if c == Server.id then
-                       j = false
-                   end
-                end
-                if j then
-                    table.insert(dontJoin,Server["id"])
-                    writefile("servers.json",httpService:JSONEncode(dontJoin))
-                    task.wait()
-                    return Server['id']
-                end
+    for _, Server in next, OtherServers["data"] do
+        if Server ~= game.JobId then
+            local j = true
+            for a,c in pairs(dontJoin) do
+               if c == Server.id then
+                   j = false
+               end
+            end
+            if j then
+                table.insert(dontJoin,Server["id"])
+                writefile("servers.json",httpService:JSONEncode(dontJoin))
+                task.wait()
+                return Server['id']
             end
         end
     end
+end
 
-    --// Script
-    local server = joinNew()
-    local function serverHop()
-        if not server then
-            writefile("servers.json",httpService:JSONEncode({}))
-            local server = joinNew()
-            teleportService:TeleportToPlaceInstance(2317712696, server)
-        else
-            teleportService:TeleportToPlaceInstance(2317712696, server)
-        end
-    end
-
-    if game:GetService("Workspace").WORKSPACE_Entities.Animals:FindFirstChild("Wendigo") then
-        loadstring(game:HttpGet"https://liteeagle.me/scripts/wildwest.lua")()
-        Notification:Notify(
-        {Title = "Success!!", Description = "Successfully Joined server With Wendigo Spawned"},
-        {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 10, Type = "default"},
-        {Image = "http://www.roblox.com/asset/?id=6403436054", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
-        )
+--// Script
+local server = joinNew()
+local function serverHop()
+    if not server then
+        writefile("servers.json",httpService:JSONEncode({}))
+        local server = joinNew()
+        teleportService:TeleportToPlaceInstance(2317712696, server)
     else
-        serverHop()
+        teleportService:TeleportToPlaceInstance(2317712696, server)
     end
+end
+
+if game:GetService("Workspace").WORKSPACE_Entities.Animals:FindFirstChild("Wendigo") then
+    loadstring(game:HttpGet"https://liteeagle.me/scripts/wildwest.lua")()
+    Notification:Notify(
+    {Title = "Success!!", Description = "Successfully Joined server With Wendigo Spawned"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 10, Type = "default"},
+    {Image = "http://www.roblox.com/asset/?id=6403436054", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
+    )
+else
+    serverHop()
 end
